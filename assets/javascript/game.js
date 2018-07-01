c393b5271ef1764e638c33cbe8ada9def4c6c71d
Eskan <39884540+Gust=-tavogonz@users.noreply.github.com>
2018-06-30 21:12:15 -0500

    added functionality to game

---
+++ assets/javascript/game.js
@@ -0,0 +1,68 @@
+
+var wordBank = ["pulpfiction", "django", "ingloriousbasterds", "true-love", "killbill"];
+var wins = 0;
+var loss = 0;
+var wrongLetter = [];
+var underScore =[];
+var guessesLeft = 9;
+var randomValue;
+var userGuess = [];
+
+
+//Set random word from array
+var randomValue = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+//changes random value to underscores
+for(var i = 0; i < randomValue.length; i++)
+{
+    underScore.push('_');
+}
+
+var answerArray = document.getElementById("word-blank");
+var answerArray = [];
+for(var i = 0; i < randomValue.length - 1; i++){
+    answerArray[i] = "_"; 
+    answerArray.push("_");
+}
+
+
+document.getElementById("word-blank").textContent = underScore.join(" ");
+
+//Remaining letters in array
+var remainingLetters = randomValue.length;
+
+
+
+//onkey function gets character from user
+document.onkeyup = function(){
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    for(var j = 0; j < randomValue.length; j++) {
+        if (randomValue[j] == userGuess){
+            answerArray[j] = userGuess;
+            remainingLetters++;
+        }
+        var el = document.getElementById("word-blank");
+        el.innerHTML = answerArray.join(" ");
+        }
+       
+  
+}
+
+
+function gameStart(){
+    while(remainingLetters > 0){
+
+
+
+        {
+           
+            }
+        } 
+    }
+
+
+
+
+
+
